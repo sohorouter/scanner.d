9b2d86d90bd84e01d2a12396a2f0c71cfe07b559
rogerA <wolfgang.schuessel@gmail.com>
2010-09-09 16:10:33 +0200

    wp

--- lib/eins.rb
+++ lib/eins.rb
@@ -1,4 +1,4 @@
-class SampleTool < Sample 
+class SampleTool 
   include CouchPotato::Persistence
 
   property :a
@@ -8,5 +8,20 @@ class SampleTool < Sample
   property :e
 
   view :all, :key => :c
+  view :show_my_range, :map => "function(doc) { if(doc.b){ c=doc.b.countryip; emit(c.id, {range:[c.start_long,c.end_long]});} }", :include_docs => true, :type => :custom
 
+  view :r1, :key => :c, :properties => [:b], :type => :properties
+  view :r2, :key => :c, :map => "function(doc) { if(doc.b){ c=doc.b.countryip; emit(c.id, {range:[c.start_long,c.end_long]});} }", :include_docs => true, :type => :raw
+
+  list :range_list, <<-JS
+    function(head,req) {
+      var row;
+      send('{"rows": [');
+      while(row=getRow()){
+        row.doc.range = row.doc.c;
+        send(JSON.stringify(row);
+      }
+      send(']}');
+    }
+  JS
 end
