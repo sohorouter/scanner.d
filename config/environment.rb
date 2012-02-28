a302b15d527699860cbda1a83e2f68aa95974062
wolfhesse <wolfgang.schuessel@gmail.com>
2012-02-28 14:54:57 +0100

    CP

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,9 @@ require File.expand_path('../application', __FILE__)
 Scanner::Application.initialize!
 
 
-CouchPotato::Config.database_name='http://192.168.0.195:5984/eins'
+#CouchPotato::Config.database_name='http://192.168.0.195:5984/eins'
 #CouchPotato::Config.database_name='http://192.168.0.196:5984/eins'
+
 require 'fileutils'
 require 'ipaddr'
 require 'pp'
